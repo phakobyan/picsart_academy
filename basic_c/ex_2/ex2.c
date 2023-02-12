982c9fab71c85d276fc7aa9418eb3ecbc6a647ff
phakobyan <petros.hakobyan@yahoo.com>
2023-02-12 23:02:35 +0400

    Last update

--- basic_c/ex_2/ex2.c
+++ basic_c/ex_2/ex2.c
@@ -5,27 +5,27 @@ int main()
 	scanf("%c", &letter);
 	if(letter >='A' && letter <= 'z')
 	{
-	switch(letter)
-	{
-		case 'a':
-		case 'e':
-		case 'i':
-		case 'o':
-		case 'u':
-		case 'y':
-		case 'A':
-		case 'E':
-		case 'I':
-		case 'O':
-		case 'U':
-		case 'Y':
-			printf("vowel letter \n ");
-			break;
-		default:
-			printf("consonant letter \n");
-			break;
+		switch(letter)
+		{
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+			case 'y':
+			case 'A':
+			case 'E':
+			case 'I':
+			case 'O':
+			case 'U':
+			case 'Y':
+				printf("vowel letter \n ");
+				break;
+			default:
+				printf("consonant letter \n");
+				break;
 
-	}
+		}
 	}
 	else
 		printf("write a character");
