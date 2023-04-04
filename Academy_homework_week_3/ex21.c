06db798b7eef846f74a24472a8e5a31807770322
Petros <petros.hakobyan@yahoo.com>
2023-04-04 14:25:23 -0400

    Week_3

--- Academy_homework_week_3/ex21.c
+++ Academy_homework_week_3/ex21.c
@@ -5,6 +5,10 @@ int main()
  int arr[] = {1, 2, 3};
  int *ptr;
  ptr = arr;
- printf("%p", ptr);
+ int res = sizeof(arr) / sizeof(arr[0]);
+ for(int i = 0; i < res; ++i)
+ {
+	printf("%i\n", *ptr+i*res);
+ }
 }
     
